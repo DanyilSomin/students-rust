5ae77c712e64208ae41c362e17f66aa07efca608
Danyil Somin <danyil.somin@gmail.com>
2022-05-05 23:43:28 +0300

    Fixed issue #7

--- src/students.rs
+++ src/students.rs
@@ -9,8 +9,11 @@ use student::Student;
 
 use crate::utils::ask_stdin_input;
 
-const COLUMN_WIDTH: usize = 20;
-const TABLE_WIDTH: usize = 38 + COLUMN_WIDTH * 4;
+const NAME_COLUMN_WIDTH: usize = 25;
+const ID_COLUMN_WIDTH: usize = 5;
+const DATE_COLUMN_WIDTH: usize = 10;
+const TABLE_WIDTH: usize =
+    38 + NAME_COLUMN_WIDTH * 2 + ID_COLUMN_WIDTH + DATE_COLUMN_WIDTH;
 
 pub trait Students {
     fn all(&self) -> Iter<'_, Student>;
@@ -23,14 +26,16 @@ pub fn as_table_to_stdout(students: Iter<'_, Student>) {
     println!("{}", "-".repeat(TABLE_WIDTH));
 
     for s in students {
-        println!("| Id: {:>width$} | Name: {:>width$} | Surname: {:>width$} | Date: {:>width$} |", 
+        println!("| Id: {:>id_width$} | Name: {:>name_width$} | Surname: {:>name_width$} | Date: {:>date_width$} |", 
             s.id(),
             s.name(),
             s.surname(),
             s.birth_date()
                 .format(student::DATE_FORMAT)
                 .to_string(),
-            width = COLUMN_WIDTH);
+            name_width = NAME_COLUMN_WIDTH,
+            id_width = ID_COLUMN_WIDTH,
+            date_width = DATE_COLUMN_WIDTH);
     }
 
     println!("{}", "-".repeat(TABLE_WIDTH));
